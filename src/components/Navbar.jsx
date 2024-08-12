9d03798077bc206bf458dff9da2fe45e8c1d396f
Himanshu sangwan <pahimanshusangwan@gmail.com>
2024-08-12 08:26:37 +0530

    Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Logo from '../assets/logo.png'
+
+const Navbar = () => {
+  return (
+    <div>
+    {/* upper side */}
+    <div>
+      <div>
+        <div>
+            <a href="#" >
+              <img src={Logo} alt="logo" />
+            </a>
+        </div>
+      </div>
+    </div>
+    {/* upper side */}
+    <div>
+
+    </div>
+
+  </div>
+  )
+}
+
+export default Navbar
